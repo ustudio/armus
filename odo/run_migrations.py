94b7ee53f171f7c30088dc884c098ea63d8d6961
Alex Tanton <tantona@gmail.com>
2015-05-07 11:43:59 -0500

    continued progress on the thing

--- odo/run_migrations.py
+++ odo/run_migrations.py
@@ -1,5 +1,7 @@
 import os
 import re
+import importlib
+# from migrations import _20120111152357_fake_migration
 
 
 migration_filter = re.compile('migration_[0-9]{14}.*')
@@ -9,13 +11,52 @@ def extract_version(name):
     try:
         version = int(name.split("_")[1])
         return version
-    #TODO make this more accurate
+    # TODO make this more accurate
     except ValueError:
         return None
 
 
 def get_migration_versions(path):
+    # TODO ABEND on duplicate versions
     migration_files = [filename for filename in os.listdir(path) if migration_filter.match(filename)]
     versions = [extract_version(filename) for filename in migration_files]
 
     return sorted(versions)
+
+
+def build_module_names(path, versions):
+    migration_files = [filename for filename in os.listdir(path) if migration_filter.match(filename)]
+
+    module_names = []
+    for filename in migration_files:
+        version = extract_version(filename)
+        if version and version in versions:
+            module_names.append(os.path.splitext(filename)[0])
+
+    return sorted(module_names, key=str.lower)
+
+
+def find_unapplied_migrations(path, applied_versions):
+    all_migration_versions = get_migration_versions(path)
+    new_migration_versions = [
+        version for version in all_migration_versions if version not in applied_versions
+    ]
+
+    migration_module_names = build_module_names(path, new_migration_versions)
+
+    return migration_module_names
+
+
+def import_module(path, module_name):
+    package_name = os.path.basename(path)
+    module = importlib.import_module(package_name + "." + module_name)
+    return module
+
+
+def run_migrations(migrations):
+    for migration in migrations:
+        migration.up()
+
+
+def test_apply_new_migrations():
+    pass
--- tests/test_run_migrations.py
+++ tests/test_run_migrations.py
@@ -1,8 +1,22 @@
+import __builtin__
 import unittest
 import os
+import sys
+import importlib
 from mock import patch, MagicMock
 
-from odo.run_migrations import extract_version, get_migration_versions
+from odo.run_migrations import extract_version, get_migration_versions, build_module_names, find_unapplied_migrations, import_module, run_migrations
+
+
+class TestModule():
+    def up(self):
+        pass
+
+    def down(self):
+        pass
+
+sys.modules['some_module'] = TestModule()
+import some_module
 
 
 class TestCreateMigrationFiles(unittest.TestCase):
@@ -16,9 +30,9 @@ class TestCreateMigrationFiles(unittest.TestCase):
         def test_get_migration_versions(self):
 
             test_directory_listing = [
-                "migration_20121016152357_first_thing",
-                "migration_20121016182212_second_thing",
-                "im_not_a_migration_file"
+                "migration_20121016152357_first_thing.py",
+                "migration_20121016182212_second_thing.py",
+                "im_not_a_migration_file.py"
             ]
 
             with patch('os.listdir', MagicMock(spec=os.listdir)) as mocklistdir:
@@ -26,3 +40,80 @@ class TestCreateMigrationFiles(unittest.TestCase):
                 result = get_migration_versions("some_path")
 
             self.assertEqual([20121016152357, 20121016182212], result)
+
+        def test_build_module_names(self):
+            test_directory_listing = [
+                "migration_20121016152357_first_thing.py",
+                "migration_20121016182212_second_thing.py",
+                "im_not_a_migration_file.py"
+            ]
+
+            with patch('os.listdir', MagicMock(spec=os.listdir)) as mocklistdir:
+                mocklistdir.return_value = test_directory_listing
+                result = build_module_names("some_path", [20121016152357, 20121016182212])
+
+            self.assertEqual(
+                ["migration_20121016152357_first_thing", "migration_20121016182212_second_thing"],
+                result
+            )
+
+        def test_find_unapplied_migrations(self):
+            no_applied_migrations = []
+            one_applied_migration = [20121016152357]
+            all_migrations_applied = [20121016152357, 20121016182212]
+
+            test_directory_listing = [
+                "migration_20121016182212_second_thing.py",
+                "migration_20121016152357_first_thing.py",
+                "im_not_a_migration_file.py"
+            ]
+
+            with patch('os.listdir', MagicMock(spec=os.listdir)) as mocklistdir:
+                mocklistdir.return_value = test_directory_listing
+                no_applied_migrations_result = find_unapplied_migrations("some_path", no_applied_migrations)
+                one_applied_migration_result = find_unapplied_migrations("some_path", one_applied_migration)
+                all_migrations_applied_result = find_unapplied_migrations("some_path", all_migrations_applied)
+
+            self.assertEqual(
+                ["migration_20121016152357_first_thing", "migration_20121016182212_second_thing"],
+                no_applied_migrations_result
+            )
+
+            self.assertEqual(
+                ["migration_20121016182212_second_thing"],
+                one_applied_migration_result
+            )
+
+            self.assertEqual([], all_migrations_applied_result)
+
+        def test_import_module(self):
+            # TODO is this testing anything?
+            with patch('importlib.import_module', MagicMock(spec=importlib.import_module)) as mockimportmodule:
+                mockimportmodule.return_value = some_module
+                module = import_module("this/is/a/path", "some_module_name")
+
+                self.assertEqual(some_module, module)
+
+        def test_run_migrations(self):
+            no_migrations = []
+            single_migration = [some_module]
+            multiple_migrations = [
+                some_module,
+                some_module
+            ]
+            with patch.object(some_module, 'up', return_value=None) as mockupmethod:
+                run_migrations(no_migrations)
+                self.assertEqual(0, mockupmethod.call_count)
+                mockupmethod.reset_mock()
+
+                run_migrations(single_migration)
+                self.assertEqual(1, mockupmethod.call_count)
+                mockupmethod.reset_mock()
+
+                run_migrations(multiple_migrations)
+                self.assertEqual(2, mockupmethod.call_count)
+                mockupmethod.reset_mock()
+
+        def test_apply_new_migrations(self):
+
+
