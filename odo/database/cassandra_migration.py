4d273d430af6eb82ca7ac64e276f71613406820c
ckrough <chris@krough.org>
2015-05-07 15:47:54 -0500

    prepare insert and select statements in db setup
    - create prepared statement for inserting a completed migration version
    - create prepared statement for selecting all completed migration versions

--- odo/database/cassandra_migration.py
+++ odo/database/cassandra_migration.py
@@ -5,12 +5,12 @@ class CassandraMigration(object):
         self.replication_factor = replication_factor
 
     def setup(self):
-
-        create_keyspace = "CREATE KEYSPACE IF NOT EXISTS {0} WITH REPLICATION = " \
-                "{{ 'class': 'SimpleStrategy', 'replication_factor': {1}}};".format(
-                    self.keyspace, self.replication_factor)
+        create_keyspace = "CREATE KEYSPACE IF NOT EXISTS {0} WITH REPLICATION = {{ 'class': 'SimpleStrategy', " \
+                          "'replication_factor': {1}}};".format(self.keyspace, self.replication_factor)
 
         self.session.execute(create_keyspace)
         self.session.execute("USE {0};".format(self.keyspace))
         self.session.execute("CREATE TABLE IF NOT EXISTS migrations ('version');")
-        # self.session.prepare()
+        self.session.prepare("INSERT INTO migrations (version) VALUES ('?');")
+        self.session.prepare("SELECT * FROM migrations ORDER BY (version ASC);")
+
