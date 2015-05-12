fa46a9f0af0318a8086c5b3de66824c913326856
Alex Tanton <tantona@gmail.com>
2015-05-12 13:52:34 -0500

    added files necessary fro pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='odo',
+  packages=['odo'],
+  version='0.1',
+  description='A tool for running scripts',
+  author='uStudio',
+  author_email='dev@ustudio.com',
+  url='https://github.com/ustudio/migration_tool',
+  download_url='https://github.com/ustudio/migration_tool/archive/0.1.tar.gz',
+  keywords=[],
+  classifiers=[],
+)
