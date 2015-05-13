55d9608139755c1b686eefca9cbaec9cd5b50f30
ckrough <chris@krough.org>
2015-05-13 12:25:08 -0500

    Fix indentation in pip setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name='armus',
-  packages=['armus'],
-  version='0.1',
-  description='A tool migrations',
-  author='uStudio',
-  author_email='dev@ustudio.com',
-  url='https://github.com/ustudio/armus',
-  download_url='https://github.com/ustudio/armus/archive/0.1.tar.gz',
-  keywords=[],
-  classifiers=[],
+    name='armus',
+    packages=['armus'],
+    version='0.1',
+    description='A tool migrations',
+    author='uStudio',
+    author_email='dev@ustudio.com',
+    url='https://github.com/ustudio/armus',
+    download_url='https://github.com/ustudio/armus/archive/0.1.tar.gz',
+    keywords=[],
+    classifiers=[],
 )
